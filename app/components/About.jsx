09c554c1d9ecf9c932a87096b7f59962675b1fbb
Jason Manns <Jason.Manns@gmx.com>
2016-09-20 22:54:06 +0100

    removed some comments

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,17 +1,8 @@
 const React = require('react');
 
-// const About = React.createClass({
-//   render() {
-//     return (
-//       <h3>About component</h3>
-//     );
-//   }
-// });
-
-// presentational components like that one above that manages no state
-// and only has a render method can be refactored to a stateless
+// presentational components(manages no state)
+// and only have a render method can be refactored to a stateless
 // functional component
-
 const About = props => {
     return (
         <h3>About component</h3>
