74e211fe8ba796abf538059afb32159d0bde1cb9
Jason Manns <Jason.Manns@gmx.com>
2016-09-20 22:28:30 +0100

    updated port

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.use((req, res, next) => {
 
 app.use(express.static('public'));
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log('express server is up on port ' + PORT);
 });
