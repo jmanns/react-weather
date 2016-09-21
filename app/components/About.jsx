41e0e773c2a63d6bd1e5855a440dc7b877d7ee03
Jason Manns <Jason.Manns@gmx.com>
2016-09-21 08:02:15 +0100

    styled about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,7 +5,27 @@ const React = require('react');
 // functional component
 const About = props => {
     return (
-        <h3>About component</h3>
+      <div>
+        <h1 className="text-center">About</h1>
+        <p>
+          This is a simple weather application built on React JS.
+          Here are some of the tools used:
+        </p>
+        <ul>
+          <li>
+            <a href="https://facebook.github.io/react">React</a> - This was the
+              Javascript framework used.
+          </li>
+          <li>
+            <a href="https://openweathermap.org">Open Weather Map</a> - The API
+               being used to search for weather data by city.
+          </li>
+          <li>
+            <a href="http://foundation.zurb.com/">Foundation</a> - was
+               used for basic styling.
+          </li>
+        </ul>
+      </div>
     );
 }
 
